a96ecc76483657f546c30cead66b3d15cf6750d6
宮内沙英 <sae@miyauchisaenoMacBook-Air.local>
2025-07-17 21:19:08 +0900

    FLOCSS見直し

--- main.js
+++ main.js
@@ -1,47 +1,17 @@
-
-// const sideMenu = document.querySelector('.c-sideMenu');
-// const menuBtn = document.querySelector('.c-menuBtn');
-// const closeIcon = document.querySelector('.c-closeIcon');
-
-
-
-
-// menuBtn.addEventListener('click', () => {
-
-//     if (sideMenu.classList.contains("close")) {
-
-//         sideMenu.classList.remove("close");
-//         sideMenu.classList.add("open");
-//         closeIcon.style.display = 'inline';
-//         menuBtn.textContent = "";
-
-
-
-//     } else {
-
-//         sideMenu.classList.remove("open");
-//         sideMenu.classList.add("close");
-//         menuBtn.textContent = 'Menu';
-//         closeIcon.style.display = 'none';
-
-//     }
-// });
-
-
-const sideMenu = document.querySelector('.c-sideMenu');
-const menuBtn = document.querySelector('.c-menuBtn');
-const closeIcon = document.querySelector('.c-closeIcon');
-const menuText = document.querySelector('.c-menuText');
+const sideMenu = document.querySelector('.l-sideMenu');
+const menuBtn = document.querySelector('.c-menuBtnSPTB');
+const closeIcon = document.querySelector('.js-closeIcon');
+const menuText = document.querySelector('.js-menuText');
 
 menuBtn.addEventListener('click', () => {
-    if (sideMenu.classList.contains("close")) {
-        sideMenu.classList.remove("close");
-        sideMenu.classList.add("open");
+    if (sideMenu.classList.contains("is-close")) {
+        sideMenu.classList.remove("is-close");
+        sideMenu.classList.add("is-open");
         menuText.style.display = 'none';
         closeIcon.style.display = 'inline-block'; // spanなのでこれ
     } else {
-        sideMenu.classList.remove("open");
-        sideMenu.classList.add("close");
+        sideMenu.classList.remove("is-open");
+        sideMenu.classList.add("is-close");
         menuText.style.display = 'inline';
         closeIcon.style.display = 'none';
     }
